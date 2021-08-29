7373adf8a0b2cfed90167d650458b77e81417ed0
chipseater_ <salutlesgas.lol@gmail.com>
2021-08-29 17:47:58 +0200

    adding json-server communication

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from './Card'
+import axios from 'axios'
 
 const Games = () => {
+    const [data, setData] = useState([])
+    
+    useEffect(() => {
+        getData()
+        // eslint-disable-next-line
+    }, []);
+
+    function getData() {
+        axios.get('http://localhost:3003/games/')
+         .then((res) => {setData(res.data)})
+    }
+    // setData(Array.from(data))
+
     return (
         <div className="games">
             <h1 className="blue">Nos Jeux</h1>
             <ul className="cards">
-                <Card name="The Night Walker" description="le MNORPG de demain" nbOfPlayers="0" />
+                {data.map((game) => (<Card key={game.id}
+                name={game.name} description={game.descr} nbOfPlayers={game.nbOfPlayers}/>))}
             </ul>
         </div>
     
